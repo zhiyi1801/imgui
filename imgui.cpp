23b0c631e47a7dd766060ed8a6cfe48fbdaf4694
zhiyiz <zhiyiz@seas.upenn.edu>
2025-04-06 22:47:05 +0800

    fix bug

--- imgui.cpp
+++ imgui.cpp
@@ -3835,11 +3835,13 @@ void ImGui::SetActiveID(ImGuiID id, ImGuiWindow* window)
 {
     ImGuiContext& g = *GImGui;
 
-
-    for (int i = 0; i < DC.Layouts.Data.Size; i++)
+    if (window != NULL)
     {
-        ImGuiLayout* layout = (ImGuiLayout*)DC.Layouts.Data[i].val_p;
-        IM_DELETE(layout);
+        for (int i = 0; i < window->DC.Layouts.Data.Size; i++)
+        {
+            ImGuiLayout* layout = (ImGuiLayout*)window->DC.Layouts.Data[i].val_p;
+            IM_DELETE(layout);
+        }
     }
     // While most behaved code would make an effort to not steal active id during window move/drag operations,
     // we at least need to be resilient to it. Cancelling the move is rather aggressive and users of 'master' branch
